89df479cdd237e070ff6e9e53551315445bfa203
Mike Goodstadt <mikegoodstadt@gmail.com>
2015-10-17 07:50:20 +0200

    Initial upload

---
+++ angular-threejs-loader.js
@@ -0,0 +1,92 @@
+/**!
+ * THREEjs Image Loader Service for
+ * THREEjs Angular module implmenting
+ * THREEjs https://github.com/mrdoob/three.js/
+ * see http://threejs.org by mrdoob
+ * @author  Mike Goodstadt  <mikegoodstadt@gmail.com>
+ * @version 1.0.0
+ */
+ 
+ (function() {
+	'use strict';
+	angular
+		.module('threejs')
+		.factory('THREELoader', THREELoader);
+
+	function THREELoader(THREEService, $document, $q, $rootScope) {
+		// check if THREEService is loaded
+		// check if plugin already loaded - add and remove form array
+		var textures = {
+			loaded: []
+		};
+
+		return {
+			load: function(filenames) {
+				var self = this;
+				var imagesToLoad = []; // push async functions into list for subsequent processing
+				angular.forEach(filenames, function(filename, key) {
+					var newImage = true;
+					for (var i = textures.loaded.length - 1; i >= 0; i--) {
+						if (textures.loaded[key] == filename) newImage = false;
+					}
+					if (newImage) {
+						var loadImage = self.add(filename);
+						imagesToLoad.push(loadImage);
+					}
+				});
+				return $q.all(imagesToLoad)
+				.then(function(results) {
+					if (results.length > 0) console.log("Images loaded: " + results);
+					return window.THREE;
+				});
+			},
+			add: function(filename) {
+				var deferred = $q.defer();
+
+				// Create Manager
+				var textureManager = new THREE.LoadingManager();
+				textureManager.onProgress = function ( item, loaded, total ) {
+					// this gets called after any item has been loaded
+					console.log( item, loaded, total );
+				};
+				textureManager.onLoad = function () {
+					// all textures are loaded
+					$rootScope.$apply(function() {
+						console.log(textures.loaded);
+						deferred.resolve(filename);
+					});
+				};
+
+				// Create New Texture
+				var newTexture = new THREE.Texture();
+				var onProgress = function ( xhr ) {
+					if ( xhr.lengthComputable ) {
+						var percentComplete = xhr.loaded / xhr.total * 100;
+						console.log( Math.round(percentComplete, 2) + '% downloaded' );
+					}
+				};
+
+				var onError = function ( xhr ) {
+				};
+				textures.loaded.push( newTexture );
+
+				// Load Image to Texture
+				var imageLoader = new THREE.ImageLoader( textureManager );
+				imageLoader.load( filename, function ( image ) {
+					newTexture.image = image;
+				}, onProgress, onError );
+
+				return deferred.promise;
+			},
+			remove: function(filename) {
+				angular.forEach(textures.loaded, function(texture, key) {
+					if (texture == filename) {
+						textures.loaded[key].pop();
+						// REMOVE DOM ELEMENT?
+						console.log("Image " + filename + " removed.");
+					}
+				});
+			}
+		};
+	}
+})();
